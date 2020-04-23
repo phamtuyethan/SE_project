d73c66894c58b85c399a37da3ccaf581513a3c71
phamtuyethan <62267047+phamtuyethan@users.noreply.github.com>
2020-04-23 18:31:25 -0500

    Update product.js

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const { body } = require('express-validator/check');
+// const { body } = require('express-validator/check');
 
 const productController = require('../controllers/product');
 
-const isAuth = require('../middleware/is-auth');
+// const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 
